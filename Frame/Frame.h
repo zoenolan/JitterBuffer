1cd5ce5968943311891c65420f27f50b9788c730
Zoe Nolan <github@zoenolan.org>
2012-11-19 08:15:17 +0000

    Make CFrame non copyable

--- Frame/Frame.h
+++ Frame/Frame.h
@@ -27,6 +27,9 @@ private:
 	std::vector<CLazyBuffer> mFragments;
 
 	int					     mFrameSizeInBytes;
+
+    CFrame( const CFrame& rhs );           // non construction-copyable
+    CFrame& operator=( const CFrame& rhs); // non copyable
 };
 
 #endif // _FRAME_H
