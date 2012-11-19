60ac1c11052fdcc99cc73e4792bfbbeaca8e4c4d
Zoe Nolan <github@zoenolan.org>
2012-11-19 07:43:15 +0000

    Added error reporting to the RenderBufferTest

--- RenderBuffer/RenderBufferTest.cpp
+++ RenderBuffer/RenderBufferTest.cpp
@@ -1,4 +1,5 @@
 
+#include <cstdio>
 #include "RenderBuffer.h"
 
 int main(int argc, char* argv[])
@@ -14,27 +15,39 @@ int main(int argc, char* argv[])
 		// test negative size
 		{-1,						  0},
 
-		// test zero sized buffer
-		{0,							  0},
-
-		// test a valid value
-		{512,						512},
-
-		// test exactly buffer sized
-		{oneMegabyte,		oneMegabyte},
-
 		// test greater than buffer sized
 		{2 * oneMegabyte,	oneMegabyte},
 	};
 
+	const char* testErrorStrings[] =
+	{
+		"Failed: Setting a negative size\n",
+		"Failed: Setting a size larger than the allocation\n",
+	};
+
 	const int numberOfTestCases = sizeof (testSize)/ (sizeof(int) * 2);
 
+	// Test out of range values
 	for (int i = 0; i < numberOfTestCases; i++)
 	{
 		renderBuffer.SetSize(testSize[i][0]);
 
 		if (renderBuffer.Size() != testSize[i][1])
 		{
+			printf("%s", testErrorStrings[i]);
+			return (-1);
+		}
+	}
+
+	// Test in range values
+	const int stepSize = oneMegabyte/1024;
+	for (int i = 0; i < oneMegabyte; i+= stepSize)
+	{
+		renderBuffer.SetSize(i);
+
+		if (renderBuffer.Size() != i)
+		{
+			printf("Failed: Setting the size to %d\n", i);
 			return (-1);
 		}
 	}
