9027abdd416f07ce6c951548133bea90ce9bafa6
Zoe Nolan <github@zoenolan.org>
2012-11-19 01:40:13 +0000

    Started Work on the body of the Frame Test

--- Frame/FrameTest.cpp
+++ Frame/FrameTest.cpp
@@ -3,8 +3,19 @@
 
 int main(int argc, char* argv[])
 {
+	CFrame frame;
+/*
+	bool Completed() const;
 
 
+	void Reset(const int numberOfFragments);
+
+	void AddFragment(const char* pBuffer,
+					 const int   length,
+					 const int   fragmentNumber);
+
+	void Combine(CLazyBuffer& outputBuffer);
+*/
 	return (0);
 }
 
