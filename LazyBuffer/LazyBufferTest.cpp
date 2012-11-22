e3cdd5c6727c313fdc7035c65496acfa2b4dae6c
Zoe Nolan <github@zoenolan.org>
2012-11-22 21:24:52 +0000

    Upgraded the lazybuffer test

--- LazyBuffer/LazyBufferTest.cpp
+++ LazyBuffer/LazyBufferTest.cpp
@@ -1,6 +1,7 @@
 
 #include <cstddef>
 #include <cstdio>
+#include <cstring> 
 #include "LazyBuffer.h"
 
 int main(int argc, char* argv[])
@@ -71,6 +72,20 @@ int main(int argc, char* argv[])
 	{
 		printf("Failed: The Buffer pointer should be NULL at the start of day\n");
 		return (-1);
+	}
+
+	memset(buffer, 1, bufferSize);
+	lazyBuffer.Copy(buffer, bufferSize);
+
+	char* pInternalBuffer = lazyBuffer.Pointer();
+
+	for (int i = 0; i < bufferSize; i++)
+	{
+		if (1 != pInternalBuffer[i])
+		{
+			printf("Failed: The Buffer contents do not match\n");
+			return (-1);
+		}
 	}
 
 	printf("Passed\n");
