18d721dba8a1855913470fac894b6fe35d40249f
Zoe Nolan <github@zoenolan.org>
2012-11-23 01:05:36 +0000

    Added a better version of the mock decoder

--- MockDecoder.cpp
+++ MockDecoder.cpp
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include <windows.h>
 
 #include "MockDecoder.h"
 
 int CMockDecoder::DecodeFrame(const char* buffer, int length, char* outputBuffer)
 {
+	Sleep(15);
 	return (length);
 }
